cedddb6c7cfee2133946bf41cd4755895bf95505
Robert Kemerer <rkemerer@sei.cmu.edu>
2021-08-26 15:47:43 -0400

    Added a Key property to the Game entity Added migration for Key property on Game

---
+++ src/Gameboard.Api/Data/Migrations/PostgreSQL/GameboardDb/20210826193257_AddGameKey.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gameboard.Api.Data.Migrations.PostgreSQL.GameboardDb
+{
+    public partial class AddGameKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Key",
+                table: "Games",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Key",
+                table: "Games");
+        }
+    }
+}
--- src/Gameboard.Api/Data/Migrations/PostgreSQL/GameboardDb/GameboardDbContextPostgreSQLModelSnapshot.cs
+++ src/Gameboard.Api/Data/Migrations/PostgreSQL/GameboardDb/GameboardDbContextPostgreSQLModelSnapshot.cs
@@ -208,6 +208,9 @@ namespace Gameboard.Api.Data.Migrations.PostgreSQL.GameboardDb
                     b.Property<bool>("IsPublished")
                         .HasColumnType("boolean");
 
+                    b.Property<string>("Key")
+                        .HasColumnType("text");
+
                     b.Property<string>("Logo")
                         .HasMaxLength(64)
                         .HasColumnType("character varying(64)");
