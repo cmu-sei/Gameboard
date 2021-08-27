b9a2e147d3285de9f2e198088968cc48b52e523e
Robert Kemerer <rkemerer@sei.cmu.edu>
2021-08-26 22:05:22 -0400

    Added 64 char limit to Game Key

---
+++ src/Gameboard.Api/Data/Migrations/SqlServer/GameboardDb/20210827020051_AddGameKeyMaxLength.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gameboard.Api.Data.Migrations.SqlServer.GameboardDb
+{
+    public partial class AddGameKeyMaxLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "Games",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64,
+                oldNullable: true);
+        }
+    }
+}
