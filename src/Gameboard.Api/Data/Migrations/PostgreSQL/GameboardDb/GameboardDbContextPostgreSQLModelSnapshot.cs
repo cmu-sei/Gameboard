3f247d8fde89b309d67eac9157f1e891502926fa
Robert Kemerer <rkemerer@sei.cmu.edu>
2021-08-26 22:05:22 -0400

    Added 64 char limit to Game Key

--- src/Gameboard.Api/Data/Migrations/PostgreSQL/GameboardDb/GameboardDbContextPostgreSQLModelSnapshot.cs
+++ src/Gameboard.Api/Data/Migrations/PostgreSQL/GameboardDb/GameboardDbContextPostgreSQLModelSnapshot.cs
@@ -209,7 +209,8 @@ namespace Gameboard.Api.Data.Migrations.PostgreSQL.GameboardDb
                         .HasColumnType("boolean");
 
                     b.Property<string>("Key")
-                        .HasColumnType("text");
+                        .HasMaxLength(64)
+                        .HasColumnType("character varying(64)");
 
                     b.Property<string>("Logo")
                         .HasMaxLength(64)
