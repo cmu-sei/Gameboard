620c06bd4b2ba1e1c576700147926b43d5bdfe54
Jeff Mattson <jmattson@sei.cmu.edu>
2021-09-06 21:22:51 -0400

    player model

--- src/Gameboard.Api/Features/Player/Player.cs
+++ src/Gameboard.Api/Features/Player/Player.cs
@@ -13,6 +13,7 @@ namespace Gameboard.Api
         public string TeamId { get; set; }
         public string UserId { get; set; }
         public string UserName { get; set; }
+        public string UserApprovedName { get; set; }
         public string GameId { get; set; }
         public string ApprovedName { get; set; }
         public string Name { get; set; }
