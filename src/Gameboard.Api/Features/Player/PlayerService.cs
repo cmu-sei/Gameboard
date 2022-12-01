5a378d2b70e1347c60a6507ae3c3d6d80238d684
Jeff Mattson <jmattson@sei.cmu.edu>
2022-12-01 12:22:43 -0500

    display player user name

--- src/Gameboard.Api/Features/Player/PlayerService.cs
+++ src/Gameboard.Api/Features/Player/PlayerService.cs
@@ -544,7 +544,9 @@ namespace Gameboard.Api.Services
                 players.First(p => p.IsManager)
             );
 
-            team.Members = Mapper.Map<TeamMember[]>(players);
+            team.Members = Mapper.Map<TeamMember[]>(
+                players.Select(p => p.User)
+            );
 
             // TODO: consider display of challenge detail after game closed
             // if (sudo || !players.First().Game.IsLive)
