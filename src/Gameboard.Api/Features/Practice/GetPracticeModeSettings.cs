8bcf145cec1ea597ad59528dc13e9635b77b672c
Ben Stein <bsstein@sei.cmu.edu>
2023-09-01 11:07:25 -0400

    allow authenticated users to retrieve practice mode settings

--- src/Gameboard.Api/Features/Practice/GetPracticeModeSettings.cs
+++ src/Gameboard.Api/Features/Practice/GetPracticeModeSettings.cs
@@ -10,20 +10,15 @@ public record GetPracticeModeSettingsQuery(User ActingUser) : IRequest<PracticeM
 
 internal class GetPracticeModeSettingsHandler : IRequestHandler<GetPracticeModeSettingsQuery, PracticeModeSettings>
 {
-    private readonly UserRoleAuthorizer _roleAuthorizer;
     private readonly IStore _store;
 
     public GetPracticeModeSettingsHandler(UserRoleAuthorizer roleAuthorizer, IStore store)
     {
-        _roleAuthorizer = roleAuthorizer;
         _store = store;
     }
 
     public Task<PracticeModeSettings> Handle(GetPracticeModeSettingsQuery request, CancellationToken cancellationToken)
     {
-        _roleAuthorizer.AllowedRoles = new UserRole[] { UserRole.Admin };
-        _roleAuthorizer.Authorize();
-
         return _store.FirstOrDefaultAsync<Data.PracticeModeSettings>(cancellationToken);
     }
 }
