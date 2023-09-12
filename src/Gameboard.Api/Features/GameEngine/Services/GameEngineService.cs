a0b8caf64115b3f71220a00289bac7f28c706870
Ben Stein <bsstein@sei.cmu.edu>
2023-09-12 16:47:44 -0400

    ACTUALLY respect StartGamespace flag (in GameEngineService).

--- src/Gameboard.Api/Features/GameEngine/Services/GameEngineService.cs
+++ src/Gameboard.Api/Features/GameEngine/Services/GameEngineService.cs
@@ -64,7 +64,7 @@ public class GameEngineService : _Service, IGameEngineService
                 {
                     Players = new RegistrationPlayer[]
                     {
-                        new RegistrationPlayer
+                        new()
                         {
                             SubjectId = registration.Player.TeamId,
                             SubjectName = registration.Player.ApprovedName
