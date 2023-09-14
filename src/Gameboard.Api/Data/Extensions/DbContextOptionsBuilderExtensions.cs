9dd66f0825f4f271a5b0ffda71a9d0c60921c1f9
Ben Stein <bsstein@sei.cmu.edu>
2023-09-14 17:27:25 -0400

    Remove logging config meta-logging - EF settings were being overridden by global Logging__LogLevel__Microsoft in the chart.

--- src/Gameboard.Api/Data/Extensions/DbContextOptionsBuilderExtensions.cs
+++ src/Gameboard.Api/Data/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -10,23 +10,19 @@ public static class DbContextOptionsBuilderExtensions
 {
     public static DbContextOptionsBuilder WithGameboardOptions(this DbContextOptionsBuilder builder, IWebHostEnvironment env, ILogger logger)
     {
-        logger.LogInformation($"""Configuring EF Logging for environment "{env.EnvironmentName}".""");
         if (env.IsDevOrTest())
         {
-            logger.LogInformation($""" In non-production-configuration "{env.EnvironmentName}". Configuring verbose logging.""");
             builder
                 .EnableDetailedErrors()
                 .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Query.Name }, LogLevel.Debug);
 
             if (env.IsDevelopment())
             {
-                logger.LogInformation($"""In Development env ("{env.EnvironmentName}"). Enabling sensitive data logging.""");
                 builder.EnableSensitiveDataLogging();
             }
         }
         else
         {
-            logger.LogInformation($"""In production environment ("{env.EnvironmentName}"). Configuring minimal logging.""");
             builder.LogTo(Console.WriteLine, LogLevel.Warning);
         }
 
