7ed3784f050d84ff46b8342252db2c32ba54d18b
Ben Stein <bsstein@sei.cmu.edu>
2023-09-18 16:11:52 -0400

    Enable sensitive data logging only in dev

--- src/Gameboard.Api/Data/Extensions/DbContextOptionsBuilderExtensions.cs
+++ src/Gameboard.Api/Data/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Gameboard.Api.Data;
@@ -14,8 +15,12 @@ public static class DbContextOptionsBuilderExtensions
             Console.WriteLine("Starting in the dev environment. Enabling detailed/sensitive EF logging...");
             builder
                 .EnableDetailedErrors()
-                .EnableSensitiveDataLogging()
                 .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Query.Name }, LogLevel.Debug);
+
+            if (env.IsDevelopment())
+            {
+                builder.EnableSensitiveDataLogging();
+            }
         }
         else
         {
