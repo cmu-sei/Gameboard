a7272610551b77c22049a2830d837ff5fa9395c6
Ben Stein <bsstein@sei.cmu.edu>
2023-10-17 14:58:59 -0400

    Pull unnecessary injection

--- src/Gameboard.Api/Features/Consoles/ConsolesController.cs
+++ src/Gameboard.Api/Features/Consoles/ConsolesController.cs
@@ -4,7 +4,6 @@ using Gameboard.Api.Common.Services;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace Gameboard.Api.Features.Consoles;
 
@@ -13,18 +12,15 @@ namespace Gameboard.Api.Features.Consoles;
 public class ConsolesController : ControllerBase
 {
     private readonly IActingUserService _actingUserService;
-    private readonly ILogger<ConsolesController> _logger;
     private readonly IMediator _mediator;
 
     public ConsolesController
     (
         IActingUserService actingUserService,
-        ILogger<ConsolesController> logger,
         IMediator mediator
     )
     {
         _actingUserService = actingUserService;
-        _logger = logger;
         _mediator = mediator;
     }
 
