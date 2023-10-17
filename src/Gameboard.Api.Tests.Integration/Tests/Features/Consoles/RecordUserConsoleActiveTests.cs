21a0c04f602a31a7cef955634c2a2ae6142f288a
Ben Stein <bsstein@sei.cmu.edu>
2023-10-17 15:12:41 -0400

    Add test and correct bug for non-extending practice sessions.

--- src/Gameboard.Api.Tests.Integration/Tests/Features/Consoles/RecordUserConsoleActiveTests.cs
+++ src/Gameboard.Api.Tests.Integration/Tests/Features/Consoles/RecordUserConsoleActiveTests.cs
@@ -44,4 +44,36 @@ public class RecordUserConsoleActiveTests
         // (See the source of RecordUserConsoleActive for a discussion about why this is currently a string)
         result.Message.ShouldBe(RecordUserConsoleActiveHandler.MESSAGE_EXTENDED);
     }
+
+    [Theory, GbIntegrationAutoData]
+    public async Task ActionRecorded_WithPracticeSessionNotNearEnd_DoesNotExtend(string userId, IFixture fixture)
+    {
+        // given
+        await _testContext.WithDataState(state =>
+        {
+            state.Add<Data.Player>(fixture, p =>
+            {
+                p.SessionBegin = DateTimeOffset.UtcNow.AddMinutes(-10);
+                // since the session end is a ways out, we don't expect the auto-extend here
+                p.SessionEnd = DateTimeOffset.UtcNow.AddMinutes(30);
+                p.Sponsor = state.Build<Data.Sponsor>(fixture);
+                p.Mode = PlayerMode.Practice;
+                p.User = state.Build<Data.User>(fixture, u => u.Id = userId);
+                p.Challenges = state.Build<Data.Challenge>(fixture, c =>
+                {
+                    c.PlayerMode = PlayerMode.Practice;
+                }).ToCollection();
+            });
+        });
+
+        // when
+        var result = await _testContext
+            .CreateHttpClientWithActingUser(u => u.Id = userId)
+            .PostAsync("api/consoles/active", null)
+            .WithContentDeserializedAs<ConsoleActionResponse>();
+
+        // then
+        // (See the source of RecordUserConsoleActive for a discussion about why this is currently a string)
+        result.Message.ShouldBe(RecordUserConsoleActiveHandler.MESSAGE_NOT_EXTENDED);
+    }
 }
