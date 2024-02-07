f78299d291a207da7751cc86f276a8125c9443b0
Ben Stein <bsstein@sei.cmu.edu>
2024-02-07 17:31:06 -0500

    Fix embedded consoles in external game

--- src/Gameboard.Api/Features/GameEngine/Services/VmUrlResolver.cs
+++ src/Gameboard.Api/Features/GameEngine/Services/VmUrlResolver.cs
@@ -25,6 +25,10 @@ internal class GameboardMksVmUrlResolver : IVmUrlResolver
             Query = $"f=1&s={vmState.IsolationId}&v={vmState.Name}"
         };
 
+        // constructing the UrlBuilder this way makes it include the port by default
+        // (so don't do that)
+        urlBuilder.Port = -1;
+
         return urlBuilder.ToString();
     }
 }
