5d2ebf0f595b21589ace8243b5da05cb46feac88
Ben Stein <bsstein@sei.cmu.edu>
2024-02-29 16:48:16 -0500

    Finish backend of 383

--- src/Gameboard.Api/Features/Admin/Requests/GetAppActiveTeams/GetAppActiveTeamsModels.cs
+++ src/Gameboard.Api/Features/Admin/Requests/GetAppActiveTeams/GetAppActiveTeamsModels.cs
@@ -1,6 +1,24 @@
 using System.Collections.Generic;
-using Gameboard.Api.Features.Teams;
 
 namespace Gameboard.Api.Features.Admin;
 
-public record GetAppActiveTeamsResponse(IEnumerable<Team> Teams);
+public record GetAppActiveTeamsResponse(IEnumerable<AppActiveTeam> Teams);
+
+public sealed class AppActiveTeam
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public required DateRange Session { get; set; }
+    public required AppActiveTeamGame Game { get; set; }
+    public required int DeployedChallengeCount { get; set; }
+    public required bool IsLateStart { get; set; }
+    public required double Score { get; set; }
+    public required double? MsRemaining { get; set; }
+}
+
+public sealed class AppActiveTeamGame
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public required bool IsTeamGame { get; set; }
+}
