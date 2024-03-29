bcd8409bd734c10fc4bcbf6e9a9d1daf29e9791d
Ben Stein <bsstein@sei.cmu.edu>
2024-03-29 15:46:22 -0400

    Remove unused game hub code.

--- src/Gameboard.Api/Features/Hubs/GameHub/GameHub.cs
+++ src/Gameboard.Api/Features/Hubs/GameHub/GameHub.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading.Tasks;
 using Gameboard.Api.Hubs;
-using Gameboard.Api.Structure;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
@@ -13,16 +12,13 @@ public interface IGameHubApi { }
 [Authorize(AppConstants.HubPolicy)]
 public class GameHub : Hub<IGameHubEvent>, IGameHubApi, IGameboardHub
 {
-    private readonly IGameHubService _gameHubService;
     private readonly ILogger<GameHub> _logger;
 
     public GameHub
     (
-        IGameHubService gameHubService,
         ILogger<GameHub> logger
     )
     {
-        _gameHubService = gameHubService;
         _logger = logger;
     }
 
@@ -39,11 +35,4 @@ public class GameHub : Hub<IGameHubEvent>, IGameHubApi, IGameboardHub
         this.LogOnDisconnected(_logger, Context, ex);
         return base.OnDisconnectedAsync(ex);
     }
-
-    // private async Task JoinGame(GameHubActiveEnrollment enrollment)
-    // {
-    //     _logger.LogInformation(LogEventId.GameHub_Group_JoinStart, message: $"""User {Context.UserIdentifier} is joining the group for game "{enrollment.Game.Id}"...""");
-    //     await Groups.AddToGroupAsync(Context.ConnectionId, enrollment.Game.Id);
-    //     _logger.LogInformation(LogEventId.GameHub_Group_JoinEnd, message: $"""User {Context.UserIdentifier} joined the group for game "{enrollment.Game.Id}".""");
-    // }
 }
