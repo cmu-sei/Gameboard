a9cd96a3297e03bcee66259ab40ec9edcdbe751e
Ben Stein <bsstein@sei.cmu.edu>
2024-04-25 15:26:44 -0400

    Fix a bug that prevented certificates from loading as intended

--- src/Gameboard.Api/Common/Tools/StartProcessAsync.cs
+++ src/Gameboard.Api/Common/Tools/StartProcessAsync.cs
@@ -7,14 +7,13 @@ public static class StartProcessAsync
 {
     public static Task<int> StartAsync(string command, string[] args = null)
     {
-
         var tcs = new TaskCompletionSource<int>();
         var startInfo = new ProcessStartInfo { FileName = command };
 
         if (args is not null)
             foreach (var arg in args) { startInfo.ArgumentList.Add(arg); }
 
-        using var process = new Process
+        var process = new Process
         {
             StartInfo = startInfo,
             EnableRaisingEvents = true
@@ -26,6 +25,7 @@ public static class StartProcessAsync
             process.Dispose();
         };
 
+        process.Start();
         return tcs.Task;
     }
 }
