8b1766d24c6f116d7c9ea47f20177721cdabbc06
Ben Stein <bsstein@sei.cmu.edu>
2024-08-21 13:09:31 -0400

    Add guarding against microsoft's weird interest in messing with claim names. Update topo client version.

--- src/Gameboard.Api/Structure/Auth/AuthorizationStartupExtensions.cs
+++ src/Gameboard.Api/Structure/Auth/AuthorizationStartupExtensions.cs
@@ -6,90 +6,82 @@ using Gameboard.Api.Auth;
 using Gameboard.Api.Structure.Auth;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class AuthorizationStartupExtensions
     {
-        public static IServiceCollection AddConfiguredAuthorization(
-            this IServiceCollection services
-        )
+        public static IServiceCollection AddConfiguredAuthorization(this IServiceCollection services)
         {
-            services.AddAuthorization(_ =>
-            {
-                _.DefaultPolicy = new AuthorizationPolicyBuilder()
+            // stops microsoft from doing weird things like renaming the "sub" claim to its own made-up value
+            JsonWebTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthorizationBuilder()
+                .SetDefaultPolicy(new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
                     .AddAuthenticationSchemes
                     (
                         JwtBearerDefaults.AuthenticationScheme,
                         ApiKeyAuthentication.AuthenticationScheme
-                    ).Build();
-
-                _.AddPolicy(AppConstants.RegistrarPolicy, new AuthorizationPolicyBuilder()
+                    ).Build())
+                .AddPolicy(AppConstants.RegistrarPolicy, new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
                     .AddAuthenticationSchemes(
                         JwtBearerDefaults.AuthenticationScheme
                     )
                     .RequireClaim(AppConstants.RoleClaimName, UserRole.Registrar.ToString())
                     .Build()
-                );
-
-                _.AddPolicy(AppConstants.DesignerPolicy, new AuthorizationPolicyBuilder()
+)
+                .AddPolicy(AppConstants.DesignerPolicy, new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
                     .AddAuthenticationSchemes(
                         JwtBearerDefaults.AuthenticationScheme
                     )
                     .RequireClaim(AppConstants.RoleClaimName, UserRole.Designer.ToString())
                     .Build()
-                );
-
-                _.AddPolicy(AppConstants.AdminPolicy, new AuthorizationPolicyBuilder()
+)
+                .AddPolicy(AppConstants.AdminPolicy, new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
                     .AddAuthenticationSchemes(
                         JwtBearerDefaults.AuthenticationScheme
                     )
                     .RequireClaim(AppConstants.RoleClaimName, UserRole.Admin.ToString())
                     .Build()
-                );
-
-                _.AddPolicy(AppConstants.TicketOnlyPolicy, new AuthorizationPolicyBuilder()
+)
+                .AddPolicy(AppConstants.TicketOnlyPolicy, new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
                     .AddAuthenticationSchemes(
                         TicketAuthentication.AuthenticationScheme
                     )
                     .Build()
-                );
-
-                _.AddPolicy(AppConstants.ConsolePolicy, new AuthorizationPolicyBuilder()
+)
+                .AddPolicy(AppConstants.ConsolePolicy, new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
                     .AddAuthenticationSchemes(
                         AppConstants.MksCookie
                     )
                     .Build()
-                );
-
-                _.AddPolicy(AppConstants.HubPolicy, new AuthorizationPolicyBuilder()
+)
+                .AddPolicy(AppConstants.HubPolicy, new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
                     .AddAuthenticationSchemes(
                         TicketAuthentication.AuthenticationScheme,
                         AppConstants.MksCookie
                     )
                     .Build()
-                );
-
-                _.AddPolicy(AppConstants.GraderPolicy, new AuthorizationPolicyBuilder()
+)
+                .AddPolicy(AppConstants.GraderPolicy, new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
                     .AddAuthenticationSchemes(
                         JwtBearerDefaults.AuthenticationScheme,
                         GraderKeyAuthentication.AuthenticationScheme
                     ).Build()
-                );
-
-                _.AddPolicy(AppConstants.ApiKeyAuthPolicy, new AuthorizationPolicyBuilder()
+)
+                .AddPolicy(AppConstants.ApiKeyAuthPolicy, new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
                     .AddAuthenticationSchemes(ApiKeyAuthentication.AuthenticationScheme)
                     .Build());
-            });
 
             return services;
         }
