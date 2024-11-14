f7696876bbfd6f3fbc76561ac36b80c85655c4ed
Ben Stein <bsstein@sei.cmu.edu>
2024-11-14 16:33:17 -0500

    Add'l team start tests

--- src/Gameboard.Api.Tests.Integration/Tests/Features/Teams/StartTeamSessionTests.cs
+++ src/Gameboard.Api.Tests.Integration/Tests/Features/Teams/StartTeamSessionTests.cs
@@ -1,5 +1,7 @@
 using Gameboard.Api.Common;
+using Gameboard.Api.Features.Teams;
 using Gameboard.Api.Structure;
+using ServiceStack;
 
 namespace Gameboard.Api.Tests.Integration.Teams;
 
@@ -10,7 +12,6 @@ public class TeamControllerStartTeamSessionTests(GameboardTestContext testContex
     [Theory, GbIntegrationAutoData]
     public async Task TeamGame_WithSinglePlayer_CantStart
     (
-        string gameId,
         string playerId,
         string userId,
         IFixture fixture
@@ -21,7 +22,7 @@ public class TeamControllerStartTeamSessionTests(GameboardTestContext testContex
         {
             state.Add(new Data.Game
             {
-                Id = gameId,
+                Id = fixture.Create<string>(),
                 MinTeamSize = 2,
                 MaxTeamSize = 5,
                 GameStart = DateTimeOffset.UtcNow,
@@ -46,7 +47,6 @@ public class TeamControllerStartTeamSessionTests(GameboardTestContext testContex
     [Theory, GbIntegrationAutoData]
     public async Task TeamGame_WithTwoPlayers_CanStart
     (
-        string gameId,
         string playerId,
         string userId,
         string teamId,
@@ -58,7 +58,7 @@ public class TeamControllerStartTeamSessionTests(GameboardTestContext testContex
         {
             state.Add(new Data.Game
             {
-                Id = gameId,
+                Id = fixture.Create<string>(),
                 MinTeamSize = 2,
                 MaxTeamSize = 5,
                 GameStart = DateTimeOffset.UtcNow,
@@ -93,4 +93,70 @@ public class TeamControllerStartTeamSessionTests(GameboardTestContext testContex
         // then we should get a player back with a nonempty session start
         result.SessionBegin.ShouldBeGreaterThan(DateTimeOffset.MinValue);
     }
+
+    [Theory, GbIntegrationAutoData]
+    public async Task TeamGame_WithCaptainPromotion_CanStart
+    (
+        string finalCaptainPlayerId,
+        string finalCaptainUserId,
+        string initialCaptainPlayerId,
+        string initialCaptainUserId,
+        string teamId,
+        IFixture fixture
+    )
+    {
+        // given a team game and a registered player with no teammates
+        await _testContext.WithDataState(state =>
+        {
+            state.Add(new Data.Game
+            {
+                Id = fixture.Create<string>(),
+                MinTeamSize = 2,
+                MaxTeamSize = 5,
+                GameStart = DateTimeOffset.UtcNow,
+                GameEnd = DateTimeOffset.UtcNow.AddDays(1),
+                Mode = GameEngineMode.Standard,
+                Players =
+                [
+                    state.Build<Data.Player>(fixture, p =>
+                    {
+                        p.Id = initialCaptainPlayerId;
+                        p.Role = PlayerRole.Manager;
+                        p.TeamId = teamId;
+                        p.User = state.Build<Data.User>(fixture, u => u.Id = initialCaptainUserId);
+                    }),
+                    state.Build<Data.Player>(fixture, p =>
+                    {
+                        p.Id = finalCaptainPlayerId;
+                        p.Role = PlayerRole.Member;
+                        p.TeamId = teamId;
+                        p.User = state.Build<Data.User>(fixture, u => u.Id = finalCaptainUserId);
+                    })
+                ]
+            });
+        });
+
+        // when they promote a new captain and then start
+        var httpClient = _testContext.CreateHttpClientWithActingUser(u => u.Id = initialCaptainUserId);
+
+        await httpClient
+            .PutAsync($"api/team/{teamId}/manager/{finalCaptainPlayerId}", new PromoteToManagerRequest
+            {
+                CurrentManagerPlayerId = initialCaptainPlayerId,
+                NewManagerPlayerId = finalCaptainPlayerId,
+                TeamId = teamId
+            }.ToJsonBody());
+
+        var result = await _testContext
+            .CreateHttpClientWithActingUser(u => u.Id = finalCaptainUserId)
+            .PutAsync($"api/player/{finalCaptainPlayerId}/start", null)
+            .DeserializeResponseAs<Player>();
+
+        // then we should get a player back with a nonempty session start
+        result.SessionBegin.ShouldBeGreaterThan(DateTimeOffset.MinValue);
+    }
+
+    // Users can team up, leave the team, join a different team, then start sessions on the original and the new team
+    // Admins can start sessions for non-admins
+    // Non-admins can't start sessions for other teams
 }
