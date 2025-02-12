1488a92369d2274d701831313c41565bb6dd67f2
Ben Stein <bsstein@sei.cmu.edu>
2025-02-12 08:44:08 -0500

    Adjust test

--- src/Gameboard.Api.Tests.Integration/Tests/Features/Practice/SearchPracticeChallengesTests.cs
+++ src/Gameboard.Api.Tests.Integration/Tests/Features/Practice/SearchPracticeChallengesTests.cs
@@ -55,7 +55,7 @@ public class SearchPracticeChallengesTests(GameboardTestContext testContext) : I
     }
 
     [Theory, GbIntegrationAutoData]
-    public async Task SearchChallenges_WithTagMatchAndWithoutSuggestedSearch_DoesNotMatch(string tag, IFixture fixture)
+    public async Task SearchChallenges_WithNonSequentialGameNameMatch_Matches(IFixture fixture)
     {
         // given a challenge spec with a tag NOT slug-equal to one of the suggested searches in 
         // the practice area...
@@ -68,23 +68,23 @@ public class SearchPracticeChallengesTests(GameboardTestContext testContext) : I
                 {
                     game.PlayerMode = PlayerMode.Practice;
                     game.IsPublished = true;
+                    game.Name = "Red Automobile Flarp";
 
                     game.Specs = state.Build<Data.ChallengeSpec>(fixture, spec =>
                     {
                         game.PlayerMode = PlayerMode.Practice;
-                        spec.Tags = tag;
                     }).ToCollection();
                 });
             }
         );
 
-        // when we search for the tag 
+        // when we search for a nonsequential match
         var result = await _testContext
             .CreateClient()
-            .GetAsync($"/api/practice?term={tag}")
+            .GetAsync($"/api/practice?term=red flarp")
             .DeserializeResponseAs<SearchPracticeChallengesResult>();
 
         // we should find it
-        result.Results.Items.Count().ShouldBe(0);
+        result.Results.Items.Count().ShouldBe(1);
     }
 }
